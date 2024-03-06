472f4e5e8ea61e58c87845bf9e21d4dba9df69cf
Fadilix <iamfadile2@gmail.com>
2024-03-06 20:59:10 +0000

    perfectly scraped quotes data

--- quotescraper/items.py
+++ quotescraper/items.py
@@ -10,3 +10,10 @@ class QuotescraperItem(scrapy.Item):
     # define the fields for your item here like:
     # name = scrapy.Field()
     pass
+
+
+class QuoteItem(scrapy.Item):
+    quote_text = scrapy.Field()
+    author = scrapy.Field()
+    tags = scrapy.Field()
+    
--- quotescraper/spiders/quote.py
+++ quotescraper/spiders/quote.py
@@ -8,10 +8,33 @@ class QuoteSpider(scrapy.Spider):
 
     def parse(self, response):
         quotes = response.css("div.quote")
-
+        
+        
         for quote in quotes:
-            yield {
-                "text" : quote.css("span.text::text").get(),
+
+            # Creating a quote item
+            quote_item = {
+                "quote_text": quote.css("span.text::text").get(),
                 "author": quote.css("span small.author::text").get(),
-                "tags": quote.css(".tags a::text").getall()
+                "tags": quote.css(".tags a::text").getall(),
             }
+
+            # url to access the author's page
+            # And get some additional information
+            url = quote.css("span a::attr(href)").get()
+            yield response.follow(url, callback=self.parse_author_page, meta={"quote_item" : quote_item})
+
+    def parse_author_page(self, response):
+
+        quote_item = response.meta.get("quote_item", {})
+
+        author_born_date =  response.css("span.author-born-date::text").get()
+        author_born_location = response.css("span.author-born-location::text").get()
+        author_description = response.css("div.author-description::text").get()
+
+        quote_item["author_born_date"] = author_born_date
+        quote_item["author_born_location"] = author_born_location
+        quote_item["author_description"] = author_description
+
+
+        yield quote_item
