8fa52bc7050db86147fe44ffbdf705e59ee2db73
Fadilix <iamfadile2@gmail.com>
2024-03-06 21:15:09 +0000

    clean code for quoteSpider

--- quotescraper/spiders/quote.py
+++ quotescraper/spiders/quote.py
@@ -1,7 +1,6 @@
 import scrapy
 from quotescraper.items import QuoteItem
 
-
 class QuoteSpider(scrapy.Spider):
     name = "quote"
     allowed_domains = ["quotes.toscrape.com"]
@@ -9,7 +8,6 @@ class QuoteSpider(scrapy.Spider):
 
     def parse(self, response):
         quotes = response.css("div.quote")
-        
         
         for quote in quotes:
 
@@ -26,7 +24,11 @@ class QuoteSpider(scrapy.Spider):
             yield response.follow(url, callback=self.parse_author_page, meta={"quote_item" : quote_item})
 
         next_page = response.css("li.next a::attr(href)").get()
-        yield response.follow(next_page, callback=self.parse)
+
+        # Continue iterating through the pages until there are no more
+        if next_page is not None:
+            yield response.follow(next_page, callback=self.parse)
+    
     def parse_author_page(self, response):
         # instance of quoteItem
         q_item = QuoteItem()
@@ -43,6 +45,5 @@ class QuoteSpider(scrapy.Spider):
         q_item["author_born_date"] = author_born_date
         q_item["author_born_location"] = author_born_location
         q_item["author_description"] = author_description
-
 
         yield q_item
