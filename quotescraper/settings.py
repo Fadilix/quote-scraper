342a8e41900da43544997bbc5f8cff26b9fac149
Fadilix <iamfadile2@gmail.com>
2024-03-08 15:38:59 +0000

    encapsulated

--- quotescraper/settings.py
+++ quotescraper/settings.py
@@ -92,3 +92,6 @@ ITEM_PIPELINES = {
 REQUEST_FINGERPRINTER_IMPLEMENTATION = "2.7"
 TWISTED_REACTOR = "twisted.internet.asyncioreactor.AsyncioSelectorReactor"
 FEED_EXPORT_ENCODING = "utf-8"
+
+
+DATABASE_URL = "{dialect}://{username}:{password}@{host}:{post}/{db_name}"
